60a0c3a2d65cd8e6c7655cc93f97d10d07605adb
Douglas Mendes <douglasleomendes@hotmail.com>
2023-12-15 11:09:45 +0000

    feat: add connector for mysql database

--- pkg/processing/processor.go
+++ pkg/processing/processor.go
@@ -1,5 +1,42 @@
 package processing
 
-func HelloWorld() string {
-	return "Hello, World!"
+import (
+	"database/sql"
+	"fmt"
+	"log"
+
+	"github.com/go-sql-driver/mysql"
+	_ "github.com/go-sql-driver/mysql"
+)
+
+type Connector struct {
+	User   string
+	Passwd string
+	Addr   string
+	DBName string
 }
+
+func ConnectToDatabase(connector Connector) {
+
+	conf := mysql.Config{
+		User:   connector.User,
+		Passwd: connector.Passwd,
+		Net:    "tcp",
+		Addr:   connector.Addr,
+		DBName: connector.DBName,
+	}
+
+	var err error
+	db, err := sql.Open("mysql", conf.FormatDSN())
+	if err != nil {
+		log.Fatal(err)
+	}
+	pingErr := db.Ping()
+	if pingErr != nil {
+		log.Fatal(pingErr)
+	}
+	fmt.Println("Connected!")
+}
+func connectToFile()          {}
+func connectToDataStreaming() {}
+func dataToObject()           {}
